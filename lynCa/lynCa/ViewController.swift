49be376a5cb3e695f72febf0946aeeea9825212f
李英男 <liyn@car-me.com>
2017-08-04 17:21:52 +0800

    新增
    新建工程

---
+++ lynCa/lynCa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lynCa
+//
+//  Created by liyingnan on 2017/8/4.
+//  Copyright © 2017年 李英男. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        print("额鹅鹅鹅鹅鹅鹅鹅鹅鹅")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
