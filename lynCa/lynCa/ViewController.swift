4f6fbb57d2cdfa7b04ed4acee79dd0fdcba26f23
李英男 <liyn@car-me.com>
2017-08-07 10:15:21 +0800

    基础

--- lynCa/lynCa/ViewController.swift
+++ lynCa/lynCa/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var Btn: UIButton!
+    var calendarButton : UIButton {
+    
+        let button = UIButton(frame: CGRect(x: 20, y: 100, width: 300, height: 50))
+        button.backgroundColor = UIColor.lightGray.withAlphaComponent(0.2)
+        button.setTitle("日期选择", for: .normal)
+        button.addTarget(self, action: #selector(handleCalendarAction(button:)), for: .touchUpInside)
+        button.setTitleColor(UIColor.black, for: .normal)
+        self.view.addSubview(button)
+
+        return button
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    
+        self.view.backgroundColor = UIColor.white
 
-        
-    
+        self.calendarButton.setTitle("ww", for: .normal)
     }
 
+
+    func handleCalendarAction(button : UIButton) {
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
